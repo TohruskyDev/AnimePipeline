8bcc14b1864606ce778507565ed4985898161522
tohru <65994850+Tohrusky@users.noreply.github.com>
2024-10-18 16:55:13 +0100

    async pool

--- tests/test_pool.py
+++ tests/test_pool.py
@@ -1,27 +1,44 @@
 import asyncio
 
+import pytest
+
 from animepipeline.pool.task import AsyncTaskExecutor
 
 
-def test_task_executor() -> None:
+async def example_task(task_id: str, duration: int) -> None:
     # Example task function
-    async def example_task(task_id: str, duration: int) -> None:
-        print(f"Task {task_id} started, will take {duration} seconds.")
-        await asyncio.sleep(duration)
-        print(f"Task {task_id} completed.")
+    print(f"Task {task_id} started, will take {duration} seconds.")
+    await asyncio.sleep(duration)
+    print(f"Task {task_id} completed.")
 
-    async def _test() -> None:
-        executor = AsyncTaskExecutor()
 
-        for task_id in range(114):
-            # Dynamically create a new task every few seconds
-            await asyncio.create_task(executor.submit_task(f"task{task_id}", example_task, f"task{task_id}", 3))
-            print(f"Task {task_id} has been submitted.")
+@pytest.mark.asyncio
+async def test_task_executor() -> None:
+    executor = AsyncTaskExecutor()
 
-        await executor.wait_all_tasks()
+    for task_id in range(114):
+        await asyncio.create_task(executor.submit_task(f"task{task_id}", example_task, f"task{task_id}", 3))
+        print(f"Task {task_id} has been submitted.")
 
-        assert len(executor.tasks) == 114
-        for task_id in range(114):
-            assert await executor.task_status(f"task{task_id}") == "Completed"
+    await executor.wait_all_tasks()
 
-    asyncio.run(_test())
+    assert len(executor.tasks) == 114
+    for task_id in range(114):
+        assert await executor.task_status(f"task{task_id}") == "Completed"
+
+
+@pytest.mark.asyncio
+async def test_task_executor_shutdown() -> None:
+    executor = AsyncTaskExecutor()
+
+    for task_id in range(114):
+        await asyncio.create_task(executor.submit_task(f"task{task_id}", example_task, f"task{task_id}", 300))
+        print(f"Task {task_id} has been submitted.")
+
+    await executor.shutdown()
+
+    print(executor.tasks)
+
+    assert len(executor.tasks) == 114
+    for task_id in range(114):
+        assert await executor.task_status(f"task{task_id}") == "Pending"
